7d99f5683283538d492cf06e9e9613e38247e146
pablorcfv <pabloruizcruces@gmail.com>
2021-05-14 12:31:14 +0200

    Añadidas clases y modificadas las interfaces
    Añado los métodos de las clases Noticias y Foro y creo las clases de Noticia y Publicación

--- infocovid/src/main/java/pilotech/interfaces/Noticias.java
+++ infocovid/src/main/java/pilotech/interfaces/Noticias.java
@@ -1,6 +1,7 @@
 package pilotech.interfaces;
 
 public class Noticias implements Display{
+    
     @Override
     public void display() {
 
@@ -10,4 +11,16 @@ public class Noticias implements Display{
     public void abrir(int id) {
 
     }
+
+    public Publicacion getPost(int id){
+
+    };
+
+    public void borrarPost(int id, Usuario user){
+
+    };
+    
+    public void verificarPost(int id, Usuario user){
+
+    };
 }
