ba63367a80791ded1b0fbe7748a34ed6acc3dce2
pablorcfv <pabloruizcruces@gmail.com>
2021-05-14 11:55:42 +0200

    Usuario.java modificado y añadidos
    Modificadas varias partes de la clase Usuario y añadidas las clases de Foro y Noticias

--- infocovid/src/main/java/pilotech/dominio/Usuario.java
+++ infocovid/src/main/java/pilotech/dominio/Usuario.java
@@ -2,29 +2,29 @@ package pilotech.dominio;
 
 import java.sql.Date;
 import java.util.Arrays;
+import java.util.List;
 
 public class Usuario extends Invitado{
     
     // Variables que guardan información sobre la cuenta del usuario
     private String nombre;
     private String correo;
-    private String contraseña;
+    private String password;
     private String ciudad;
     private int id;
     private Date fechaReg;
-    private int numPosts; // Hará falta hacer un getPost(id) ?
-    private int[] idPosts;
+    private List<Integer> idPosts;
 
-    public Usuario(String nombre, String correo, String contraseña, String ciudad, int id, Date fechaReg){
+    public Usuario(String nombre, String correo, String password, String ciudad, int id, Date fechaReg){
+        super();
         this.setRegistered();
         this.nombre = nombre;
         this.correo = correo;
-        this.contraseña = contraseña;
+        this.password = password;
         this.ciudad = ciudad;
         this.id = id;
         this.fechaReg = fechaReg;
-        this.numPosts = 0;
-        this.idPosts = new int[0];
+        this.idPosts = new ArrayList<Integer>();
     }
     
     public String getNombre() {
@@ -43,12 +43,12 @@ public class Usuario extends Invitado{
         this.correo = correo;
     }
 
-    public String getContraseña() {
-        return contraseña;
+    public String getpassword() {
+        return password;
     }
 
-    public void setContraseña(String contraseña) {
-        this.contraseña = contraseña;
+    public void setpassword(String password) {
+        this.password = password;
     }
 
     public String getCiudad() {
@@ -83,19 +83,23 @@ public class Usuario extends Invitado{
         this.numPosts = numPosts;
     }
 
-    public int[] getIDPosts(){
+    public List<Integer> getIDPosts(){
         return this.idPosts;
     }
 
     // Comprueba si el usuario ha creado la publicación indicada por su id
     public boolean hasPost(int id){
-        return Arrays.binarySearch(this.idPosts, id) > 0;
+        return this.idPosts.contains(id);
     }
 
+    // Añade la id de un post a la lista de publicaciones del usuario
     public void addIDPost(int id){
-        // TODO Añadir la id de un post a la lista de publicaciones del usuario
-        
-        this.numPosts++;
+        idPosts.add(id);
+    }
+
+    // Borra la id de un post a la lista de publicaciones del usuario
+    public void removeIDPost(int id){
+        idPosts.remove(idPosts.indexOf(id));
     }
 
 }
---
+++ infocovid/src/main/java/pilotech/interfaces/Noticias.java
@@ -0,0 +1,1 @@
+package pilotech.interfaces;
