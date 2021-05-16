2026701029298cc0b13a1d5f0f9ce84940fb2243
firehellrain <pabloruizcruces@gmail.com>
2021-05-16 12:30:37 +0200

    Añadida Noticias.java a la DB

--- infocovid/src/main/java/es/pilotech/infocovid/interfaces/Noticias.java
+++ infocovid/src/main/java/es/pilotech/infocovid/interfaces/Noticias.java
@@ -1,10 +1,24 @@
 package es.pilotech.infocovid.interfaces;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import es.pilotech.infocovid.dominio.Noticia;
 import es.pilotech.infocovid.dominio.Publicacion;
 import es.pilotech.infocovid.dominio.Usuario;
 
+@Entity
 public class Noticias implements Display{
     
+    @Id
+    @GeneratedValue
+    private int id;
+    
+    @ManyToOne
+    private Noticia listaNoticias;
+
     @Override
     public void display() {
 
