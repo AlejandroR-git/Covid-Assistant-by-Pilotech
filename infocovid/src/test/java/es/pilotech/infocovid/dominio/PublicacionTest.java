b6c73adbf3a29e088add6803fdbd9fb8851d1db8
jcarlosgarciaa <80254558+jcarlosgarciaa@users.noreply.github.com>
2021-06-06 17:31:02 +0200

    PublicacionTest Realizado
    Clase PublicacionTest

--- infocovid/src/test/java/es/pilotech/infocovid/dominio/PublicacionTest.java
+++ infocovid/src/test/java/es/pilotech/infocovid/dominio/PublicacionTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class PublicacionTest {
+	Publicacion publicacion;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -26,9 +27,59 @@ class PublicacionTest {
 	void tearDown() throws Exception {
 	}
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	@BeforeEach
+	public void init() {
+		publicacion = new Publicacion();
 	}
+	
+	@AfterEach
+	public void terminate() {
+		publicacion = null;
+	}
+	
+	@Test
+	void trasEstablecerUnIdEseIdQuedaAlmacenadoEnLaPublicacion() {
+		int id = 1;
+		publicacion.setId(id);
+		assertEquals(id,publicacion.getId());
+	}
+	@Test
+	void trasEstablecerUnTituloEseTituloQuedaAlmacenadoEnLaPublicacion() {
+		String titulo = "Titulo de la publicacion";
+		publicacion.setTitulo(titulo);
+		assertEquals(titulo,publicacion.getTitulo());
+	}
+
+
+
+	@Test
+	void trasEstablecerUnCuerpoEseCuerpoQuedaAlmacenadoEnLaPublicacion() {
+		String cuerpo = "Este es el cuerpo de la publicacion";
+		publicacion.setCuerpo(cuerpo);
+		assertEquals(cuerpo,publicacion.getCuerpo());
+	}
+
+	@Test
+	void trasEstablecerUnAutorEseAutorQuedaAlmacenadoEnLaPublicacion() {
+		String autor= "Este es el autor de la publicacion";
+		publicacion.setAutor(autor);
+		assertEquals(autor,publicacion.getAutor());
+	}
+
+
+	@Test
+	void trasEstablecerQueUnaPublicacionEstaVerificadaSeQuedaAlmacenado() {
+		boolean verificado = true;
+		publicacion.setVerificado(verificado);
+		assertEquals(verificado,publicacion.isVerificado());
+	}
+
+	@Test
+	void trasEstablecerUnUserIdEseIdQuedaAlmacenadoEnLaPublicacion() {
+		int id = 1;
+		publicacion.setUserID(id);
+		assertEquals(id,publicacion.getUserID());
+	}
+
 
 }
